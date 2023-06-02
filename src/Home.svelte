0d55a23d7aaf474aff710ce560fe2cd2913d7264
Cloudys <cloudysdeveloper@gmail.com>
2023-06-02 23:40:32 +0700

    update

---
+++ src/Home.svelte
@@ -0,0 +1,8 @@
+<script>
+  
+</script>
+<body>
+    <div class="text-center flex items-center justify-center">
+        Selamat datang<span class="inline-block text-red-500">HEwllo</span> di portfolio ini
+    </div>
+</body>
