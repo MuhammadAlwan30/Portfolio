6658f495c926477dfaca5400aa0e43e25519bce1
Cloudys <cloudysdeveloper@gmail.com>
2023-06-04 09:12:52 +0700

    add first layout

--- src/Home.svelte
+++ src/Home.svelte
@@ -5,13 +5,18 @@
     }
 </script>
 
-<header class="m-0 p-4 w-full flex flex-row-reverse h-16 bg-green-500"><nav class="flex justify-center mx-2 w-1/2">
-    <a href="#home" class="block mx-2 text-gray-800" >Home</a>
-    <a href="#about" class="block mx-2 text-gray-800">about</a>
-    <a href="#blog" class="block mx-2 text-gray-800">blog</a>
-
-</nav>
-
-    </header>
-
+<header class="m-0 p-4 w-full flex flex-row justify-between bg-indigo-500 h-[10vh]">
+    <div class="order-1"><h1 class="text-2xl font-bold heading">Allawan</h1></div>
+    <nav class=" order-2 justify-center mx-2 w-1/2">
+        <ul class="flex justify-center items-center">
+            <li class="mx-2 text-black hover:bg-blue-500 hover:text-white py-1 px-2 rounded-md block"><a href="#home">Home</a></li>
+            <li class="mx-2 text-black hover:bg-blue-500 hover:text-white px-1 py-2 rounded-md block"><a href="#about">About</a></li>
+            <li class="mx-2 text-black hover:bg-blue-500 hover:text-white px-1 py-2 rounded-md block"><a href="#about">project</a></li>
+            <li class="mx-2 text-black hover:bg-blue-500 hover:text-white px-1 py-2 rounded-md block"><a href="#about">contact</a></li>
+        </ul>
+    </nav>
+</header>
+<main class="w-full min-h-[90vh] bg-indigo-400 grid place-items-center">
+    <p>Hello my name is <span class="inline-block font-mono text-xl">Alwan</span></p>
+</main>
 
