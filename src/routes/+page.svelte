795ad3f4201440b1ded6c60eda43c3dd014b6100
Cloudys <cloudysdeveloper@gmail.com>
2023-06-13 11:34:54 +0700

    add barebone project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    <div id="wrapper">
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+</body>
+    <style>
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+    body{
+        background-color: #4c4cc1;
+        font-family:"fira mono", monospace;
+    }
+    #wrapper{
+        display: grid;
+        place-items:center;
+        min-height: 100vh;
+        min-width: 100vw;
+    }
+</style>
