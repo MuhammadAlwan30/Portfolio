6867391299a89a518be16886592bfb9df4a1dc48
Cloudys <cloudysdeveloper@gmail.com>
2023-06-13 14:29:30 +0700

    update interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
-<body>
+<script>
+import "../app.css"
+</script>
+    <body>
+    <header class="flex justify-between items-center h-1/4 min-w-[100vw]">
+        <div id="logo" class="p-4 mx-2 flex cursor-pointer"><img src="logo.svg" alt="logoimage" srcset=""> Alwan</div>
+        <nav class="w-60">
+            <ul class="flex flex-row w-full justify-between">
+                <li class="inline-block"><a href="#home">Home</a></li>
+                <li class="inline-block"><a href="#about">About</a></li>
+                <li class="inline-block"><a href="#Project">Project</a></li>
+                <li class="inline-block"><a href="blog">Blog</a></li>
+            </ul>
+        </nav>
+        <div id="contact">
+            <a href="https://twitter.com/Cloudysdev" target="_blank" rel="noopener noreferrer">
+                <button class="mx-4 bg-emerald-500 rounded-md p-1">contact</button>
+        </a>
+        </div>
+    </header>
     <div id="wrapper">
-        <h1>Welcome to SvelteKit</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+        <h1>Hello I'm Alwan</h1>
+        <h2>front end developer who enthusisast in web development and recent tech trends</h2>
     </div>
-</body>
-    <style>
-    *{
-        margin:0;
-        padding:0;
-        box-sizing: border-box;
+    </body>
+    <style lang="postcss">
+    h1{
+        font-size:4rem;
     }
     body{
         background-color: #4c4cc1;
@@ -17,7 +34,7 @@
     #wrapper{
         display: grid;
         place-items:center;
-        min-height: 100vh;
+        min-height: 90vh;
         min-width: 100vw;
     }
 </style>
