93b43a2e2e8601633a74771f7e9a5655bf048ff1
Cloudys <cloudysdeveloper@gmail.com>
2023-06-13 14:32:55 +0700

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@ import "../app.css"
 </script>
     <body>
     <header class="flex justify-between items-center h-1/4 min-w-[100vw]">
-        <div id="logo" class="p-4 mx-2 flex cursor-pointer"><img src="logo.svg" alt="logoimage" srcset=""> Alwan</div>
+        <div id="logo" class="p-4 mx-2 flex cursor-pointer"><img src="logo.svg" alt="logoimage" srcset="" class="mx-2"> Alwan</div>
         <nav class="w-60">
             <ul class="flex flex-row w-full justify-between">
                 <li class="inline-block"><a href="#home">Home</a></li>
@@ -20,12 +20,15 @@ import "../app.css"
     </header>
     <div id="wrapper">
         <h1>Hello I'm Alwan</h1>
-        <h2>front end developer who enthusisast in web development and recent tech trends</h2>
+        <h2>i'm front end developer who enthusiast in web development and recent tech news and trends</h2>
     </div>
     </body>
     <style lang="postcss">
     h1{
         font-size:4rem;
+    }
+    h2{
+        font-size: 1.5em;
     }
     body{
         background-color: #4c4cc1;
